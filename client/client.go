4ab1a3169fd43ed162b88f57438afcc25e5e8b20
Yasitha Gamage <yasithamaxs@gmail.com>
2023-08-23 17:14:03 +0530

    Add UDP Client & Server

---
+++ client/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	serverAddr, err := net.ResolveUDPAddr("udp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error resolving server address:", err)
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	message := []byte("Hello, UDP Server!")
+	_, err = conn.Write(message)
+	if err != nil {
+		fmt.Println("Error sending data:", err)
+		os.Exit(1)
+	}
+
+	buffer := make([]byte, 1024)
+	bytesRead, _, err := conn.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println("Error receiving data:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Response from server:", string(buffer[:bytesRead]))
+}
